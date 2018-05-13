8eee6208acb26beb546c18de8656c5565645169d
rishflab <rishflab@hotmail.com>
2018-05-13 20:38:06 +1000

    generic implementation update state working with point2d

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,29 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add};
 
 fn square(x: i32) -> i32 {
     x*x
 }
 
+#[derive(Debug, PartialEq, Clone)]
+enum Command<T: Add> {
+    Translate(Point2d<T>),
+    Shoot,
+}
 
-//enum Command<T> {
-//    translate(Point2d<T>),
-//    shoot
-//}
 
+#[derive(Debug, PartialEq, Clone)]
+struct State<T: Add> {
+    ammo: u32,
+    position: Point2d<T>,
+}
 
-
-//
-//#[derive(Debug, PartialEq)]
-//struct State<T> {
-//    ammo: u32,
-//    position: Point2d<T>,
-//}
-
-#[derive(Debug, PartialEq)]
-struct Point2d<T> where
-    T: Add {
+#[derive(Debug, PartialEq, Clone)]
+struct Point2d<T: Add> {
     x: T,
     y: T,
 }
 
-impl <T> Add for Point2d<T> where T: Add<Output = T> {
+impl <T> std::ops::Add for Point2d<T> where T: Add<Output = T> {
 
     type Output = Point2d<T>;
 
@@ -37,6 +34,27 @@ impl <T> Add for Point2d<T> where T: Add<Output = T> {
         }
     }
 }
+
+
+fn update_state<T:Add<Output = T>>(state: State<T>, command: Command<T>) -> State<T> {
+
+    match command {
+        Command::Shoot => {
+            State {
+                ammo: state.ammo - 1,
+                position: state.position,
+            }
+        },
+        Command::Translate(p) => {
+            State {
+                ammo: state.ammo,
+                position: state.position + p,
+            }
+        }
+    }
+}
+
+
 
 
 #[cfg(test)]
@@ -57,16 +75,51 @@ mod tests {
         assert_eq!(p3, Point2d {x: 3, y: 7});
     }
 
-//
-//    #[test]
-//    fn calculate_new_state(state: State, commands: Vec<Command>) -> State {
-//        for command in commands.iter() {
-//            match command {
-//                &Command::shoot => {state.ammo -= 1},
-//                &Command::translate(x,y) => {state.position.x = },
-//            }
-//        }
-//    }
+    #[test]
+    fn test_add_point_to_state() {
+        let s1 = State {
+            ammo: 10,
+            position: Point2d {
+                x: 0.0,
+                y: 0.0,
+            }
+        };
+
+        let p1 = Point2d{x:2.1, y:3.2};
+
+        let s2 = State {
+            ammo: 10,
+            position: p1 + s1.position,
+        };
+        assert_eq!(s2, State {ammo: 10, position: Point2d {x: 2.1, y: 3.2}});
+    }
+
+    #[test]
+    fn test_update_state() {
+
+        let s1 = State {
+            ammo: 10,
+            position: Point2d {
+                x: 0.0,
+                y: 0.0,
+            }
+        };
+
+        let s2 = State {
+            ammo: 10,
+            position: Point2d {
+                x: -1.4, y: 3.5
+            }
+        };
+
+        let c1 = Command::Translate(Point2d {x: -1.4, y: 3.5});
+
+        let new_state = update_state(s1, c1);
+        assert_eq!(new_state, s2 )
+    }
+
+
+
 
 
 }
