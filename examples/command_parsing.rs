4873f40612ee06a183abbd8586b0767c7a92bd1e
rishflab <rishflab@hotmail.com>
2018-07-21 10:26:46 +1000

    added command consolidation

--- examples/command_parsing.rs
+++ examples/command_parsing.rs
@@ -21,18 +21,34 @@ impl Component for Position {
     type Storage = VecStorage<Self>;
 }
 
-struct MovePosition {
-
-}
-
 struct CommandBuffer {
     move_commands: Vec<MoveCommand>,
+}
+
+impl CommandBuffer {
+    fn new() -> Self {
+        CommandBuffer {
+            move_commands: Vec::new(),
+        }
+    }
+}
+
+impl Default for CommandBuffer {
+      fn default() -> Self {
+        Self::new()
+    }
+}
+
+
+
+struct MovePosition {
+
 }
 
 impl<'a> System<'a> for MovePosition {
     type SystemData = (Entities<'a>, ReadStorage<'a, MoveCommand>, WriteStorage<'a, Position>);
 
-    fn run(&self, (entities, move_commands, mut positions): Self::SystemData) {
+    fn run(&mut self, (entities, move_commands, mut positions): Self::SystemData) {
         for (entity, move_command, position) in (&*entities, &move_commands, &mut positions).join() {
             if position.0 + move_command.translate < 10.0 {
                 position.0 += move_command.translate;
@@ -42,11 +58,35 @@ impl<'a> System<'a> for MovePosition {
     }
 }
 
-struct CommandAllocator;
+struct CommandAllocator {
 
+}
+
+//impl Default for CommandAllocator {
+//      fn default() -> Self {
+//        Self::new()
+//    }
+//}
 
 impl<'a> System<'a> for CommandAllocator {
-    type SystemData = (Read<>, ReadStorage<'a, MoveCommand>, )
+
+    type SystemData = (Read<'a, CommandBuffer>, WriteStorage<'a, MoveCommand>);
+
+    fn run(&mut self, (command_buffer, mut move_command):Self::SystemData) {
+
+        for command in command_buffer.move_commands.iter() {
+
+            let entity = command.entity;
+
+            let mut entity_move_command = move_command.get_mut(entity).unwrap();
+
+            let mut move_command = *command_buffer.move_commands.get_mut(0).unwrap();
+
+            entity_move_command = move_command;
+
+
+        }
+    }
 
 }
 
@@ -56,7 +96,7 @@ fn main() {
     let mut world = World::new();
     world.register::<Position>();
     world.register::<MoveCommand>();
-
+    world.add_resource(CommandBuffer::new());
 
     let player1 = world.create_entity()
         .with(Position(10.0))
